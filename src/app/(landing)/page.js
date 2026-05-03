b2a896d61ed030b121ca6742fb35c28ddc22a15b
Shourov98 <mdmahbub0412@gmail.com>
2026-05-03 12:07:59 +0600

    feat (landing page): cta section design

--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -3,6 +3,7 @@ import HowItWorks from "@/components/landing_page/HowItWorks";
 import Features from "@/components/landing_page/Features";
 import UserRoles from "@/components/landing_page/UserRoles";
 import TrustSection from "@/components/landing_page/TrustSection";
+import CTASection from "@/components/landing_page/CTASection";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Features />
       <UserRoles />
       <TrustSection />
+      <CTASection />
     </main>
   );
 }
